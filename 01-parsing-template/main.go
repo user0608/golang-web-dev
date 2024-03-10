4d2b4a014ee149dee307a9e54a591a058fda40fa
Kevin Saucedo <user002_@outlook.com>
2024-03-09 20:21:12 -0500

    simple template parsing

---
+++ 01-parsing-template/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("tpl.html")
+	if err != nil {
+		slog.Error("error loading templates", "error", err)
+		os.Exit(1)
+	}
+	if err := tpl.Execute(os.Stdout, nil); err != nil {
+		slog.Error("error executing template", "error", err)
+		os.Exit(1)
+	}
+}
